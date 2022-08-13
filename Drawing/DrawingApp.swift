6cf7c394cf1893b55452c1b0246142e5975732e6
Burak Cüce <b.cuece@web.de>
2022-08-13 18:56:48 +0200

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Burak Cüce on 13.08.22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
